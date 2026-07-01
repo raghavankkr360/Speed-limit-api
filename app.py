295fc065e2c0c9fd2d4908c95da10b7fba2c51e7
raghavankkr360 <raghavankkr360@gmail.com>
2026-07-01 19:53:41 +0530

    Connect Render API to Supabase

--- app.py
+++ app.py
@@ -1,22 +1,48 @@
-from flask import Flask
-import pandas as pd
-
-# Load datasets
-chennai = pd.read_csv("Chennai_Speed_Limit.csv")
-thiruvallur = pd.read_csv("Thiruvallur_speed_limit.csv")
+from flask import Flask, request, jsonify
+import requests
 
 app = Flask(__name__)
+
+SUPABASE_URL = "https://qwrlfojltqfkgyjjhwky.supabase.co"
+SUPABASE_KEY = "sb_publishable_wY6i_JE-2Msitn-vkCkJWA_SPoSpSUm"
 
 @app.route("/")
 def home():
     return "Speed Limit API Running"
 
-@app.route("/count")
-def count():
-    return {
-        "chennai": len(chennai),
-        "thiruvallur": len(thiruvallur)
+@app.route("/speedlimit")
+def speedlimit():
+
+    lat = float(request.args.get("lat"))
+    lon = float(request.args.get("lon"))
+
+    headers = {
+        "apikey": SUPABASE_KEY,
+        "Authorization": f"Bearer {SUPABASE_KEY}"
     }
+
+    url = f"{SUPABASE_URL}/rest/v1/Speed%20limit?select=X,Y,Speed_Limit"
+
+    response = requests.get(url, headers=headers)
+
+    data = response.json()
+
+    nearest = None
+    min_distance = 999999999
+
+    for row in data:
+        distance = (
+            (float(row["X"]) - lon) ** 2 +
+            (float(row["Y"]) - lat) ** 2
+        )
+
+        if distance < min_distance:
+            min_distance = distance
+            nearest = row
+
+    return jsonify({
+        "speed_limit": nearest["Speed_Limit"]
+    })
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
