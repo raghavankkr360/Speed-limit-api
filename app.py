3bbed3209155f560e81b54df32ea5456ce1418ea
raghavankkr360 <raghavankkr360@gmail.com>
2026-07-01 18:03:03 +0530

    Add test endpoint

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return "Speed Limit API Running"
 
+@app.route("/test")
+def test():
+    return "Test OK"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
